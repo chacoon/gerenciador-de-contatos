ac14f052cc7803b8b5e4361cf891c2dac6e5aeb7
ERIK CHACON DA SILVA <15160066@CFP127ALUNO.LOCAL>
2016-07-25 15:46:52 -0300

    first commit

---
+++ Gerenciamento de contatos/src/br/com/contato/helper/MySQL_Connect.java
@@ -0,0 +1,26 @@
+package br.com.contato.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQL_Connect {
+	public static Connection conectar_db(){
+
+		Connection con = null;
+
+		try {
+			//Especificando o driver de conexão MYSQL
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//String de conexão com o ip do banco, o banco de dados usados, usuario e senha
+			con = DriverManager.getConnection("jdbc:mysql://10.107.134.57/contatos", "root", "root");
+
+		} catch (ClassNotFoundException | SQLException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+}
---
+++ Gerenciamento de contatos/src/br/com/contato/view/Main.java
@@ -0,0 +1,24 @@
+package br.com.contato.view;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Contato.fxml"));
+ 		primaryStage.setScene(new Scene(root));
+ 		primaryStage.setTitle("Gerenciamento de contatos");
+ 		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
